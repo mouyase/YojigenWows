9af638d0532ad2025b4523a197172bc4dc128192
mouyase <mouyase@users.noreply.github.com>
2020-08-05 17:26:52 +0000

    deploy: e03d0295aa232fbfd0e0f7c7e748701b44844a2e

--- js/chunk-2d22d746.fbc349a4.js
+++ js/chunk-2d22d746.fbc349a4.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,e){"use strict";e.r(t);var s=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"about"})},u=[],a=e("2877"),c={},l=Object(a["a"])(c,s,u,!1,null,null,null);t["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d22d746.7c13b40f.js.map
+//# sourceMappingURL=chunk-2d22d746.fbc349a4.js.map
